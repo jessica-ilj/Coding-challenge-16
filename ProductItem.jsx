fadf39099744a038a7db9c95474ae758a786e86e
Jessica Ilok <142093879+jessica-ilj@users.noreply.github.com>
2024-11-17 22:08:33 -0500

    Create ProductItem component to display individual product details

---
+++ ProductItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProductItem({ name, price, description }) { //function to display the price.
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Price: ${price}</p> 
+      <p>Description: {description}</p>
+    </div>
+  );
+}
+
+export default ProductItem;
